2e84ceec2fdb35f821d439ce45db0ab3de44d0b8
arczewski <artur.czapiewski@protonmail.com>
2024-02-28 14:10:54 +0100

    Move battle button to the center of the screen

--- Assets/BattleGame/Scripts/Battle/BattleManager.cs
+++ Assets/BattleGame/Scripts/Battle/BattleManager.cs
@@ -93,7 +93,7 @@ namespace AFSInterview
         {
             if (!battleStarted)
             {
-                if (GUI.Button(new Rect(0, 0, 100, 30), "StartBattle"))
+                if (GUI.Button(new Rect((Screen.width / 2) - 50, Screen.height / 2, 100, 30), "StartBattle"))
                 {
                     StartBattle();
                 }
